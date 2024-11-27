dda32d17bec0a297099d1d5ed16a1b5da4e3fcb1
clipChronicle <disej2354@gmail.com>
2024-11-27 21:16:16 +0530

    added new file

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -4,10 +4,11 @@ import Image from "next/image";
 import Link from "next/link";
 import App from "next/app";
 import { AppointmentForm } from "@/components/forms/AppointmentForm";
+import { getPatient } from "@/lib/actions/patient.actions";
 
-
-export default function NewAppointment() {
-  return (
+export default async function NewAppointment({params: { userId}}: SearchParamProps) {
+    const patient = await getPatient(userId);
+    return (
     <div className="flex h-screen max-h-screen">
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[860px] flex-1 justify-between" >
@@ -19,7 +20,11 @@ export default function NewAppointment() {
            className="mb-12 h-100 w-fit"
           />
 
-          <AppointmentForm />
+          <AppointmentForm 
+            type="create"
+            userId={userId}
+            patientId={patient.$id}
+          />
 
           <p className="justify-tems-end text-dark-600 xl:text-left">
           © 2024 MedPulse 
